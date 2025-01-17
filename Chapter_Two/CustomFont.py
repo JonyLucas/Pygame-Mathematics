1b2bac06394291ffc0a9be695625edae519a07ba
João Lucas <jony.lucas.br13@gmail.com>
2025-01-17 13:03:41 -0300

    - Text and fonts on pygame

---
+++ Chapter_Two/CustomFont.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.font.init()
+print(pygame.font.get_fonts())
+font = pygame.font.SysFont('fonts/SLICKYBOHEM-Regular.otf', 120, True, False)
+text = font.render('Hello World', False, (255, 255, 255))
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.blit(text, (100, 100))
+    pygame.display.update()
+
+pygame.quit()
